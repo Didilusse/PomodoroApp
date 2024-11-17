6f69f7e63a65690ee167c25754fe23c740032966
Adil Rahmani <didilusse@gmail.com>
2024-11-17 12:15:22 -0500

    Initial Commit

---
+++ PomodoroApp/PomodoroAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PomodoroAppApp.swift
+//  PomodoroApp
+//
+//  Created by Adil Rahmani on 11/17/24.
+//
+
+import SwiftUI
+
+@main
+struct PomodoroAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
